052fc358f053a7c48f21ecbfb7d7ae16e00c67d2
LebedevaAlena <loik-alena@mail.ru>
2016-11-30 01:54:47 +0200

    realized class

--- line.h
+++ line.h
@@ -6,11 +6,16 @@ class line
 {
     public:
         line();
+        line(double k1, double k2, double c1, double c2);
         virtual ~line();
+        void setValue(double k1, double k2, double c1, double c2);
+        friend void printValue(line &lin);
+        bool perpend();
 
     protected:
 
     private:
+        double y1, y2, k1, k2, c1, c2, x;
 };
 
 #endif // LINE_H

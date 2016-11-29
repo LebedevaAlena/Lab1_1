a5a9bf776d59b121c372eaa23610350e5402d439
LebedevaAlena <loik-alena@mail.ru>
2016-11-30 01:58:16 +0200

    added void setValue and constructor

--- line.cpp
+++ line.cpp
@@ -1,5 +1,6 @@
 #include "line.h"
-
+#include <iostream>
+using namespace std;
 line::line()
 {
     //ctor
@@ -8,4 +9,18 @@ line::line()
 line::~line()
 {
     //dtor
+}
+
+line::line(double k1, double k2, double c1, double c2)
+: k1(k1), k2(k2), c1(c1), c2(c2)
+{
+
+}
+
+void line::setValue(double k1, double k2, double c1, double c2)
+{
+this->c1 = c1;
+this->c2 = c2;
+this->k1 = k1;
+this->k2 = k2;
 }

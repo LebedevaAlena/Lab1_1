8bdee5bb0e9461660b95c8935a5c4fcd9a116258
LebedevaAlena <loik-alena@mail.ru>
2016-11-30 02:04:35 +0200

    realized case

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include "line.h"
-
 using namespace std;
 
+void Case(line *lin1, line *lin2)
+{
+    cout << " line 1: ";
+    printValue(*lin1);
+    cout << "line 2: ";
+    printValue(*lin2);
+}
 int main()
 {
-    line a(2, 8, 8, 3);
-    printValue(a);
+    line *lin1 = new line(2, 8, 8, 3);
+    line *lin2 = new line(2, -8, 8, -3);
+    cout << "Case 1:" << endl;
+    Case(lin1, lin2);
+    cout << endl;
+
+    delete lin1;
+    delete lin2;
+
+    lin1 = new line(1, -1, 2, 3);
+    lin2 = new line(1, -5, -5, 5);
+    cout << "Case 1:" << endl;
+    Case(lin1, lin2);
+    cout << endl;
+
+    delete lin1;
+    delete lin2;
+    /*line a(2, 8, 8, 3);
+    printValue(a);*/
     return 0;
 }

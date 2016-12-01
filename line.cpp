7b737ce1147126bd566173ab94c0e6e4f3763210
LebedevaAlena <loik-alena@mail.ru>
2016-12-01 22:35:58 +0200

    Fixed

--- line.cpp
+++ line.cpp
@@ -11,32 +11,33 @@ line::~line()
     //dtor
 }
 
-line::line(double k1, double k2, double c1, double c2)
-: k1(k1), k2(k2), c1(c1), c2(c2)
+line::line(double k, double c)
+: k(k), c(c)
 {
 
 }
 
-void line::setValue(double k1, double k2, double c1, double c2)
+line::line(line &lin)
+: k(lin.k), c(lin.c)
 {
-this->c1 = c1;
-this->c2 = c2;
-this->k1 = k1;
-this->k2 = k2;
+
 }
 
-bool line::perpend()
+double line::Perpendicular(double k1, double c1, double k2, double c2)
 {
-    y1 = k1 * x + c1;
-    y2 = k2 * x + c2;
-     if(k1 * k2 == -1)
-         cout << "perpend" << endl;
-        else cout << " ne perpend" << endl;
+    return k1 * k2;
+}
+
+bool line::perpend(line &lin)
+{
+    double P = Perpendicular(k, c, lin.k, lin.c);
+     if( P == -1) return true;
+        else return false;
 
 }
 
 void printValue(line &lin)
 {
-    cout << "k1=" << lin.k1 << '\t' << "k2=" << lin.k2 << endl;
-    lin.perpend();
+    cout << "k=" << lin.k << '\t' << "c=" << lin.c << endl;
+    lin.perpend(lin);
 }
